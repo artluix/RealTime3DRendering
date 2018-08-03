8b4320ba65273ecac6d57a037174607016a98965
artluix <artluix@gmail.com>
2018-08-04 01:07:47 +0300

    Make tools generate independently

--- generate.bat
+++ generate.bat
@@ -4,12 +4,17 @@ set "SOURCE_FOLDER=%~dp0\src"
 set "BUILD_FOLDER=%~dp0\build"
 set "GENERATOR=Visual Studio 15 2017"
 
+set TARGET=APPLICATION
+if "%1" == "tools" (
+    set TARGET=TOOLS
+)
+
 pushd %BUILD_FOLDER%
-cmake -G "%GENERATOR%" %SOURCE_FOLDER%
+cmake -G "%GENERATOR%" %SOURCE_FOLDER% -D%TARGET%=TRUE
 popd
 
 echo.
-echo Generated!
+echo %TARGET% solution generated!
 echo.
 
 pause
